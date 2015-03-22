8520f201fad3ae87c2d15340a18761ffee83a3bc
Ivan Habunek <ivan.habunek@gmail.com>
2015-03-22 18:57:47 +0100

    Made CFP link go to announcement, not application form

--- cfp/urls.py
+++ cfp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from cfp.views import PaperApplicationCreateView, PaperApplicationUpdateView
 
 urlpatterns = patterns('',
-    url(r'^$', 'cfp.views.cfp_announcement'),
+    url(r'^$', 'cfp.views.cfp_announcement', name='cfp_announcement'),
     url(r'^new/$', PaperApplicationCreateView.as_view(), name='application_create'),
     url(r'^(?P<cfp_id>\d+)/new$', PaperApplicationCreateView.as_view(), name='application_create'),
     url(r'^(?P<cfp_id>\d+)/application/(?P<pk>\d+)$', PaperApplicationUpdateView.as_view(), name='application_update'),
