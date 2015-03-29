0dd19371830a0d4f574a945832c216627142c902
Ivan Nikolić <niksy5@gmail.com>
2015-03-29 16:51:52 +0200

    Adjust logo and icon display

--- Gruntfile.js
+++ Gruntfile.js
@@ -141,6 +141,22 @@ module.exports = function ( grunt ) {
                 },
                 tasks: ['watch:css','watch:js']
             }
+        },
+
+        svgstore: {
+            sprite: {
+                options: {
+                    prefix: 'Sprite-',
+                    includeTitleElement: false,
+                    preserveDescElement: false
+                },
+                files: {
+                    'ui/static/images/sprite.svg': [
+                        'ui/static/images/icons/social/**/*.svg',
+                        'ui/static/images/icons/user.svg'
+                    ]
+                }
+            }
         }
 
     });
