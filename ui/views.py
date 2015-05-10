b32150647e656371056dcadfd75c00194cb850b1
Deni Bertovic <deni@denibertovic.com>
2015-05-10 21:34:21 +0200

    pep8 fix

--- ui/views.py
+++ ui/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 def code_of_conduct(request):
     return render(request, 'ui/code_of_conduct.html', {})
 
+
 def signup_success(request):
     return render(request, 'account/signup-success.html')
+
 
 def blog_temp(request):
     return render(request, 'ui/blog_temp.html', {})
