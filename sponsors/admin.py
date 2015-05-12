00d8200ec084c33cb70911870f49436ac9bb843e
Deni Bertovic <deni@denibertovic.com>
2015-05-12 15:04:28 +0200

    fix sponsor field order in admin

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -4,6 +4,7 @@ from .models import Sponsor
 
 
 class SponsorAdmin(admin.ModelAdmin):
+    fields = ('name', 'slug', 'type', 'about', 'url', 'image', 'is_active')
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'type')
 
