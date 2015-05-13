ccdf08cde8ffe1c467abcd13753dff01d03e6f0d
Deni Bertovic <deni@denibertovic.com>
2015-05-13 11:20:55 +0200

    add active flag for sponsors admin

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -6,7 +6,7 @@ from .models import Sponsor
 class SponsorAdmin(admin.ModelAdmin):
     fields = ('name', 'slug', 'type', 'about', 'url', 'image', 'is_active')
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ('name', 'type')
+    list_display = ('name', 'type', 'is_active')
 
 
 admin.site.register(Sponsor, SponsorAdmin)
