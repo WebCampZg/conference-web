3e673a28818361d9911684673235e5ac134bf72f
Deni Bertovic <deni@denibertovic.com>
2015-05-15 09:42:27 +0200

    refactor blog views a bit
    make sure we have sponsors on the single post view
    as well.

--- blog/views.py
+++ blog/views.py
@@ -5,21 +5,34 @@ from sponsors.choices import SPONSOR_TYPES
 from .models import Post
 
 
+def get_sponsors():
+    diamond_sponsors = Sponsor.objects.filter(
+        is_active=True).filter(type=SPONSOR_TYPES.DIAMOND)
+    track_sposors = Sponsor.objects.filter(
+        is_active=True).filter(type=SPONSOR_TYPES.TRACK)
+    standard_sponsors = Sponsor.objects.filter(
+        is_active=True).filter(type=SPONSOR_TYPES.STANDARD)
+    supporter_sponsors = Sponsor.objects.filter(
+        is_active=True).filter(type=SPONSOR_TYPES.SUPPORTER)
+    return {'diamond_sponsors': diamond_sponsors,
+            'track_sponsors': track_sposors,
+            'standard_sponsors': standard_sponsors,
+            'supporter_sponsors': supporter_sponsors}
+
+
 def list_posts(request):
-    diamond_sponsors = Sponsor.objects.filter(is_active=True).filter(type=SPONSOR_TYPES.DIAMOND)
-    track_sposors = Sponsor.objects.filter(is_active=True).filter(type=SPONSOR_TYPES.TRACK)
-    standard_sponsors = Sponsor.objects.filter(is_active=True).filter(type=SPONSOR_TYPES.STANDARD)
-    supporter_sponsors = Sponsor.objects.filter(is_active=True).filter(type=SPONSOR_TYPES.SUPPORTER)
+    sponsors = get_sponsors()
     posts = Post.objects.all()
-    return render(request, 'blog/list_posts.html', {
-        'posts': posts,
-        'diamond_sponsors': diamond_sponsors,
-        'track_sponsors': track_sposors,
-        'standard_sponsors': standard_sponsors,
-        'supporter_sponsors': supporter_sponsors})
+
+    ctx = {}
+    ctx['posts'] = posts
+    ctx.update(sponsors)
+
+    return render(request, 'blog/list_posts.html', ctx)
 
 
 def view_post(request, slug):
+    sponsors = get_sponsors()
     post = get_object_or_404(Post, slug=slug)
     try:
         previous = Post.get_previous_by_created_at(post)
@@ -31,8 +44,11 @@ def view_post(request, slug):
     except Post.DoesNotExist:
         next = None
 
-    return render(request, 'blog/view_post.html', {
-        'post': post,
-        'previous': previous,
-        'next': next})
+    ctx = {}
+    ctx['post'] = post
+    ctx['previous'] = previous
+    ctx['next'] = next
+    ctx.update(sponsors)
+
+    return render(request, 'blog/view_post.html', ctx)
 
