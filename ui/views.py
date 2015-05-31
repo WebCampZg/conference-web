48b5033dbe381cebda5271bdfd51ab317497a348
Ivan Nikolić <niksy5@gmail.com>
2015-05-31 20:05:12 +0200

    Initial work on talks listing

--- ui/views.py
+++ ui/views.py
@@ -18,8 +18,3 @@ def code_of_conduct(request):
 
 def signup_success(request):
     return render(request, 'account/signup-success.html')
-
-
-def blog_temp(request):
-    return render(request, 'ui/blog_temp.html', {})
-
