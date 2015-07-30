42f6048238270a7e9aee256084ab18f7ded13ffe
Deni Bertovic <deni@denibertovic.com>
2015-07-30 19:52:14 +0200

    Adds better talks admin interface

--- talks/admin.py
+++ talks/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from django.core import urlresolvers
+
 from .models import Talk
 
 
-admin.site.register(Talk)
+class TalkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'link_to_applicant', 'skill_level', 'duration', 'keynote')
+
+    def link_to_applicant(self, obj):
+        link = urlresolvers.reverse("admin:cfp_applicant_change", args=[obj.application.applicant.id])
+        return u'<a href="%s">%s</a>' % (link, obj.application.applicant)
+    link_to_applicant.allow_tags = True
+
+admin.site.register(Talk, TalkAdmin)
 
