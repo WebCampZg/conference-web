8e1bf4f619b11e2348beb3c3d0f3abe012c24f8c
Ivan Nikolić <ivan.nikolic@trikoder.net>
2015-09-01 09:53:49 +0200

    Use bundler for Sass

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Watcher doesn’t work properly on newer version
+gem "sass", "3.4.4"
--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function ( grunt ) {
         sass: {
             dist: {
                 options: {
+                    bundleExec: true,
                     sourcemap: 'none',
                     style: 'expanded',
                     precision: 5,
