06acfa2f75ac63290988c4e22e72aa237dd26824
Ivan Nikolić <niksy5@gmail.com>
2015-09-08 20:50:21 +0200

    Add sponsor detailview

--- ui/static/javascripts/source/index.js
+++ ui/static/javascripts/source/index.js
@@ -3,5 +3,5 @@ require('./components/share-popup').init();
 require('./components/site-nav').init();
 require('./components/bithub').init();
 require('./components/highlights-nav').init();
-require('./components/vote-talk').init();
 require('./components/track-outbound-links').init();
+require('./components/job').init();
