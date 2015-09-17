226c78d0ae9de1c0f5c73c8890cabe0a06a64811
Deni Bertovic <deni@denibertovic.com>
2015-09-17 11:42:28 +0200

    Adds missing sponsor types to context

--- ui/ctx.py
+++ ui/ctx.py
@@ -8,15 +8,24 @@ def get_sponsors():
         is_active=True).filter(type=SPONSOR_TYPES.DIAMOND)
     track_sposors = Sponsor.objects.filter(
         is_active=True).filter(type=SPONSOR_TYPES.TRACK)
+    foodanddrinks_sposors = Sponsor.objects.filter(
+        is_active=True).filter(type=SPONSOR_TYPES.FOOD_AND_DRINKS)
     standard_sponsors = Sponsor.objects.filter(
         is_active=True).filter(type=SPONSOR_TYPES.STANDARD)
     supporter_sponsors = Sponsor.objects.filter(
         is_active=True).filter(type=SPONSOR_TYPES.SUPPORTER)
+    mainmedia_sponsors = Sponsor.objects.filter(
+        is_active=True).filter(type=SPONSOR_TYPES.MAIN_MEDIA)
+    media_sponsors = Sponsor.objects.filter(
+        is_active=True).filter(type=SPONSOR_TYPES.MEDIA)
 
     return {'diamond_sponsors': diamond_sponsors,
             'track_sponsors': track_sposors,
+            'foodanddrinks_sposors_sponsors': foodanddrinks_sposors,
             'standard_sponsors': standard_sponsors,
-            'supporter_sponsors': supporter_sponsors}
+            'supporter_sponsors': supporter_sponsors,
+            'mainmedia_sponsors': mainmedia_sponsors,
+            'media_sponsors': media_sponsors}
 
 
 def sponsors(request):
