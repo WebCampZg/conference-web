85d205d183a272152932622b99cd194b3895f2da
vedran <vedran.vojvoda@gmail.com>
2015-09-26 20:50:30 +0200

    Added blog and job rss feeds

---
+++ jobs/feeds.py
@@ -0,0 +1,20 @@
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse_lazy
+from jobs.models import Job
+
+
+class JobFeed(Feed):
+    title = 'WebCampZG Job Board'
+    link = reverse_lazy('jobs_list_jobs')
+
+    def item_link(self, item):
+        return item.url
+
+    def items(self):
+        return Job.objects.order_by('?').all()
+
+    def item_title(self, item):
+        return '%s - %s' % (item.sponsor.name, item.title)
+
+    def item_description(self, item):
+        return item.text
