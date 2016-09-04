5688d4b11552b46cb0afeff881abf971b7cbf222
Ivan Habunek <ivan.habunek@gmail.com>
2016-09-04 16:54:38 +0200

    Add usergroup representatives

--- usergroups/models.py
+++ usergroups/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from people.models import User
 from utils.behaviors import Permalinkable
 
 
@@ -8,7 +9,7 @@ class UserGroup(Permalinkable):
     meetup_url = models.URLField(max_length=255, blank=True)
     image = models.FileField(max_length=255, null=True, blank=True, upload_to='uploads/usergroups/')
     is_active = models.BooleanField(default=True)
+    representatives = models.ManyToManyField(User)
 
     def __unicode__(self):
         return self.name
-
