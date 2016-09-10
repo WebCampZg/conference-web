bbf62254ef351c9983fe276f49012d99355a78d7
Ivan Habunek <ivan.habunek@gmail.com>
2016-09-10 17:18:10 +0200

    Link talk to sponsor and usergroup
    Display those on the frontend.

--- talks/admin.py
+++ talks/admin.py
@@ -4,9 +4,6 @@ from django.core import urlresolvers
 from .models import Talk
 
 
-def mark_as_sponsored(modeladmin, request, queryset):
-    queryset.update(is_sponsored=True)
-
 
 def mark_as_community_chosen(modeladmin, request, queryset):
     queryset.update(is_community_chosen=True)
@@ -14,14 +11,14 @@ def mark_as_community_chosen(modeladmin, request, queryset):
 
 class TalkAdmin(admin.ModelAdmin):
     list_display = ('title', 'link_to_applicant', 'skill_level', 'duration',
-            'keynote', 'is_sponsored', 'is_community_chosen')
+            'keynote', 'sponsor', 'is_community_chosen')
 
     def link_to_applicant(self, obj):
         link = urlresolvers.reverse("admin:cfp_applicant_change", args=[obj.application.applicant.id])
         return u'<a href="%s">%s</a>' % (link, obj.application.applicant)
     link_to_applicant.allow_tags = True
 
-    actions = [mark_as_sponsored, mark_as_community_chosen]
+    actions = [mark_as_community_chosen]
 
     def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
         field = super(TalkAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
--- talks/models.py
+++ talks/models.py
@@ -1,33 +1,31 @@
 from django.db import models
+from django.db.models.deletion import PROTECT
 from django.template.defaultfilters import slugify
 
-from cfp.models import PaperApplication, AudienceSkillLevel, Applicant
-from utils.behaviors import Timestampable
 from cfp.choices import TALK_DURATIONS
+from cfp.models import PaperApplication, AudienceSkillLevel, Applicant
+from usergroups.models import UserGroup
+from utils.behaviors import Timestampable
+from sponsors.models import Sponsor
 
 
 class Talk(Timestampable):
-    application = models.OneToOneField(PaperApplication, related_name='talk')
-
-    co_presenter = models.ForeignKey(Applicant, related_name='co_talks',
-            blank=True, null=True)
+    application = models.OneToOneField(PaperApplication, PROTECT, related_name='talk')
+    co_presenter = models.ForeignKey(Applicant, PROTECT, blank=True, null=True, related_name='co_talks')
+    sponsor = models.ForeignKey(Sponsor, PROTECT, blank=True, null=True, related_name="sponsored_talks")
+    usergroup = models.ForeignKey(UserGroup, PROTECT, blank=True, null=True, related_name="chosen_talks")
 
     title = models.CharField(max_length=255, blank=True)
     about = models.TextField(blank=True)
     abstract = models.TextField(blank=True)
     skill_level = models.ForeignKey(AudienceSkillLevel, blank=True, null=True)
-    duration = models.CharField(
-            choices=TALK_DURATIONS,
-            max_length=255,
-            blank=True,
-            null=True)
+    duration = models.CharField(choices=TALK_DURATIONS, max_length=255, blank=True, null=True)
     slug = models.SlugField(blank=True, max_length=255, null=True)
     slides_url = models.URLField(blank=True)
     rate_url = models.URLField(blank=True)
     youtube_id = models.CharField(blank=True, max_length=20)
 
     keynote = models.BooleanField(default=False)
-    is_sponsored = models.BooleanField(default=False)
     is_community_chosen = models.BooleanField(default=False)
 
     def get_absolute_url(self):
