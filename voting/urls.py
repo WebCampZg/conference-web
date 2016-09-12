0a00597df1d9262a1a76428e60c883e7a0be7b86
Ivan Habunek <ivan.habunek@gmail.com>
2016-09-12 10:07:22 +0200

    Fix voting urls to remove deprecations warnings

--- voting/urls.py
+++ voting/urls.py
@@ -1,19 +1,18 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.conf import settings
 
+from .views import voting, add_vote, remove_vote
 
-urlpatterns = patterns('voting.views',
-    url(r'^$', 'voting',
-        name='voting_index'),
-    url(r'^(?P<vote_token>[0-9]+)/$', 'voting',
-        name='voting_index'),
-)
+
+urlpatterns = [
+    url(r'^$', voting, name='voting_index'),
+    url(r'^(?P<vote_token>[0-9]+)/$', voting, name='voting_index'),
+]
 
 if settings.VOTING_ENABLED:
-    urlpatterns += patterns('voting.views',
-        url(r'^vote/add/(?P<application_id>[0-9]+)/$', 'add_vote',
+    urlpatterns += [
+        url(r'^vote/add/(?P<application_id>[0-9]+)/$', add_vote,
             name='voting_add_vote'),
-        url(r'^vote/rm/(?P<application_id>[0-9]+)/$', 'remove_vote',
+        url(r'^vote/rm/(?P<application_id>[0-9]+)/$', remove_vote,
             name='voting_add_vote'),
-    )
-
+    ]
