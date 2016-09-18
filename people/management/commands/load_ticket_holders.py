36dd1944c77330eb75e31963b8b5512b2d6e18f9
Ivan Habunek <ivan.habunek@gmail.com>
2016-09-18 22:46:44 +0200

    Use urllib instead of requests to fetch entrio data
    to get around a problem with SNI in py 2.7.6
    http://docs.python-requests.org/en/master/community/faq/#what-are-hostname-doesn-t-match-errors

--- people/management/commands/load_ticket_holders.py
+++ people/management/commands/load_ticket_holders.py
@@ -1,4 +1,8 @@
 import logging
+import sys
+
+from json import loads
+from urllib2 import urlopen, URLError
 
 from django.core.management.base import NoArgsCommand
 from django.contrib.auth import get_user_model
@@ -26,37 +30,35 @@ class Command(NoArgsCommand):
 
         url = "https://www.entrio.hr/api/get_visitors?key=%s&format=json" % settings.ENTRIO_API_KEY
         try:
-            r = requests.get(url)
-            if r.status_code != 200:
-                logging.error(
-                    'Unexpected response from Entrio API. Status code: {0}'.format(r.status_code)
-                )
-            else:
-                ticket_holders = r.json()
-                for user in ticket_holders:
-                    email = user['E-mail'].strip()
-                    ticket_code = user['ticket_code'].strip()
-                    try:
-                        with transaction.atomic():
-                            u = UserModel.objects.create_user(
-                                email=email,
-                                first_name=user['First name'],
-                                last_name=user['Last name'],
-                                password=ticket_code)
-                            group.user_set.add(u)
+            response = urlopen(url)
+            ticket_holders = loads(response.read())
+            self.process_ticket_holders(group, ticket_holders)
+        except URLError as e:
+            logging.error(e)
+            sys.stderr.write("Failed loading entrio data: %r" % e)
 
-                    except IntegrityError as e:
-                        u = UserModel.objects.get(email=email)
-                        if not u.is_ticket_holder:
-                            group.user_set.add(u)
+    def process_ticket_holders(self, group, ticket_holders):
+        for user in ticket_holders:
+            email = user['E-mail'].strip()
+            ticket_code = user['ticket_code'].strip()
+            try:
+                with transaction.atomic():
+                    u = UserModel.objects.create_user(
+                        email=email,
+                        first_name=user['First name'],
+                        last_name=user['Last name'],
+                        password=ticket_code)
+                    group.user_set.add(u)
 
-                    token, created = VoteToken.objects.get_or_create(
-                        user=u,
-                        defaults={
-                            'ticket_code': ticket_code,
-                            'user': u,
-                        }
-                    )
+            except IntegrityError as e:
+                u = UserModel.objects.get(email=email)
+                if not u.is_ticket_holder:
+                    group.user_set.add(u)
 
-        except requests.exceptions.RequestException as e:
-            logging.error('Requests error when talking to the Entrio API: {0}'.format(e))
+            token, created = VoteToken.objects.get_or_create(
+                user=u,
+                defaults={
+                    'ticket_code': ticket_code,
+                    'user': u,
+                }
+            )
