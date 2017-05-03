4f36d73d7368ec02f472f0f4a077f8033a2f0fb0
Ivan Habunek <ivan@habunek.com>
2017-05-03 13:34:27 +0200

    Better user profile pages

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from people.views import UserProfileView
+from people.views import UserProfileView, UserProfileUpdateView
 from project.sitemaps import sitemaps
 
 handler404 = 'ui.views.custom_404'
@@ -13,21 +13,28 @@ handler404 = 'ui.views.custom_404'
 urlpatterns = [
     url(r'^$', ui.views.index, name="ui_index"),
     url(r'^404/', ui.views.custom_404),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^signup/success/$', ui.views.signup_success),
-    url(r'^jobs/', include('jobs.urls')),
-    url(r'^sponsors/', include('sponsors.urls')),
-    url(r'^cfp/', include('cfp.urls')),
+
+    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    # Account views
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^accounts/profile/', UserProfileView.as_view(), name='user_profile'),
+    url(r'^accounts/profile/$', UserProfileView.as_view(), name='user_profile'),
+    url(r'^accounts/profile/update/$', UserProfileUpdateView.as_view(), name='user_profile_update'),
+
+    # Apps
+    url(r'^blog/', include('blog.urls')),
+    url(r'^cfp/', include('cfp.urls')),
+    url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
+    url(r'^jobs/', include('jobs.urls')),
+    url(r'^schedule/', include('schedule.urls')),
+    url(r'^sponsors/', include('sponsors.urls')),
+    url(r'^talks/', include('talks.urls')),
+    url(r'^voting/', include('voting.urls')),
+
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
         name='django.contrib.sitemaps.views.sitemap'),
-    url(r'^talks/', include('talks.urls')),
-    url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
-    url(r'^schedule/', include('schedule.urls')),
-    url(r'^voting/', include('voting.urls')),
 ]
 
 if settings.DEBUG:
