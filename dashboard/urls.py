8638bedaa4758ea9e40a1bcb5940288ade036d7b
Ivan Habunek <ivan@habunek.com>
2017-05-31 08:34:58 +0200

    Restyle the dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.DashboardView.as_view(), name='dashboard'),
-    url(r'^applications/(?P<pk>\d+)/$', views.CallForPapersView.as_view(), name='call_for_papers'),
+    url(r'^applications/(?P<pk>\d+)/$', views.CallForPapersView.as_view(), name='call-for-papers'),
     url(r'^applications/(?P<cfp_id>\d+)/(?P<pk>\d+)/$', views.ApplicationDetailView.as_view(), name='application_detail'),
     url(r'^community-vote/$', views.CommunityVoteView.as_view(), name='community-vote'),
     url(r'^event/(?P<pk>\d+)/$', views.EventDetailView.as_view(), name='event-detail'),
