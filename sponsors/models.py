e6a4307db557b55eaf048b2a3022b6264046eedb
Ivan Habunek <ivan@habunek.com>
2018-04-27 13:14:08 +0200

    Use markdown instead of HTMLField for sponsors

--- sponsors/models.py
+++ sponsors/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 
-from tinymce.models import HTMLField
-
 from utils.behaviors import Permalinkable
 from .choices import SPONSOR_TYPES
 
-class SponsorManager(models.Manager):
 
+class SponsorManager(models.Manager):
     def active(self):
-        return self.get_queryset().filter(is_active=True)
+        return self.filter(is_active=True)
 
 
 class Sponsor(Permalinkable):
     name = models.CharField(max_length=255, unique=True)
-    type = models.CharField(max_length=255,
-            choices=SPONSOR_TYPES,
-            default=SPONSOR_TYPES.STANDARD)
-    about = HTMLField()
+    type = models.CharField(max_length=255, choices=SPONSOR_TYPES, default=SPONSOR_TYPES.STANDARD)
+    about = models.TextField()
     url = models.URLField(max_length=255)
     image = models.FileField(max_length=255, null=True, blank=True, upload_to='uploads/sponsors/')
     is_active = models.BooleanField(default=False)
