91f1e1f60bd5159fbea2c3e0b27c0ad57974ca42
Ivan Habunek <ivan@habunek.com>
2018-04-27 12:17:37 +0200

    Remove legacy community vote models

--- voting/models.py
+++ voting/models.py
@@ -1,33 +1,8 @@
-from django.conf import settings
-from django.contrib.sites.shortcuts import get_current_site
 from django.db import models
 from django.db.models.deletion import CASCADE
-from django.urls import reverse_lazy
 
 from cfp.models import PaperApplication
 from events.models import Ticket
-
-
-class Vote(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
-    application = models.ForeignKey(PaperApplication, on_delete=CASCADE, related_name='votes')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = (("user", 'application'),)
-
-
-class VoteToken(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE)
-    ticket_code = models.CharField(max_length=255)
-    token_sent = models.DateTimeField(null=True, blank=True)
-
-    @property
-    def absolute_vote_url(self):
-        return '{0}{1}'.format(
-            get_current_site(request=None).domain,
-            reverse_lazy('voting_index', kwargs={'vote_token': self.ticket_code})
-        )
 
 
 class CommunityVote(models.Model):
