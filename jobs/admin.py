852decb796ea14ff64a046a61ed5876ca5543896
Ivan Habunek <ivan@habunek.com>
2018-04-27 13:26:00 +0200

    Add Job.is_active to admin fields

--- jobs/admin.py
+++ jobs/admin.py
@@ -5,9 +5,9 @@ from .models import Job
 
 class JobAdmin(admin.ModelAdmin):
     search_fields = ('title',)
-    list_filter = ('title', 'sponsor',)
-    fields = ('title', 'text', 'url', 'slug', 'sponsor', 'location')
-    list_display = ('title', 'sponsor', 'url')
+    list_filter = ('is_active', 'sponsor',)
+    fields = ('title', 'text', 'url', 'slug', 'sponsor', 'location', 'is_active')
+    list_display = ('title', 'sponsor', 'url', 'is_active')
     ordering = ('-created_at',)
     prepopulated_fields = {'slug': ('title',)}
 
