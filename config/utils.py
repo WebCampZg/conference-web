94ce13322498d2aba79733c0d5643c30c38f6580
Ivan Habunek <ivan@habunek.com>
2019-03-02 22:46:16 +0100

    Cache site config

--- config/utils.py
+++ config/utils.py
@@ -1,6 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
 def get_site_config():
     from .models import SiteConfig
     return SiteConfig.load()
+
+
+@receiver(post_save, sender="config.SiteConfig")
+def clear_site_cache(sender, **kwargs):
+    """Clears site config cache on any change."""
+    get_site_config.cache_clear()
 
 
 def get_active_event():
