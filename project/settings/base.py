634d14e875950f180d2049d37df9e2e5b8363f06
Ivan Habunek <ivan@habunek.com>
2019-04-04 09:11:36 +0200

    Override default date and datetime formats
    Required disabling l10n

--- project/settings/base.py
+++ project/settings/base.py
@@ -137,10 +137,12 @@ TIME_ZONE = 'Europe/Zagreb'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+DATE_FORMAT = 'Y-m-d'
+DATETIME_FORMAT = 'Y-m-d @ H:i'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
