ee13f46fc14ae71b32edb71c194cd912fca88953
Ivan Habunek <ivan@habunek.com>
2019-04-05 13:54:27 +0200

    Serve media files in debug mode

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
@@ -42,3 +43,4 @@ if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
